77107c45834707a323b3249f145dee18e5cdcaf7
Matt McVey <matt.mcvey49@gmail.com>
2021-01-04 11:04:11 -0700

    Intial commit with iteration 0 finished

---
+++ index.js
@@ -0,0 +1,9 @@
+var questionInput = document.querySelector("#question");
+var getAnswerButton = document.querySelector("get-your-answer-button");
+var clearButton = document.querySelector(".clear");
+
+questionInput.addEventListener("click", displayAnswer);
+
+function displayAnswer() {
+  
+}
